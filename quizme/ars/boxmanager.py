ff0b48c23ee73f3ea71d00a3c574f7f7891d9ab8
Ben Greenberg <Greenbbm@mail.uc.edu>
2024-10-29 16:21:52 -0400

    Update boxmanager.py

--- quizme/ars/boxmanager.py
+++ quizme/ars/boxmanager.py
@@ -1,1 +1,41 @@
 """Module for managing boxes in the Adaptive Review System."""
+from datetime import timedelta
+from box import Box
+from question import Question
+
+class BoxManager:
+    def __init__(self):
+        self._boxes = [
+            Box("Missed Questions", timedelta(seconds=60)),
+            Box("Unasked Questions", timedelta(seconds=0)),
+            Box("Correctly Answered Once", timedelta(seconds=180)),
+            Box("Correctly Answered Twice", timedelta(seconds=360)),
+            Box("Known Questions", timedelta.max)
+        ]
+        self._question_location = {}
+
+    def add_new_question(self, question):
+        self._boxes[1].add_question(question)
+        self._question_location[str(question.id)] = 1
+
+    def move_question(self, question, answered_correctly):
+        current_index = self._question_location[str(question.id)]
+        if answered_correctly:
+            new_index = min(current_index + 1, len(self._boxes) - 1)
+        else:
+            new_index = 0
+        self._boxes[current_index].remove_question(question)
+        self._boxes[new_index].add_question(question)
+        self._question_location[str(question.id)] = new_index
+        self._log_box_counts()
+
+    def get_next_question(self):
+        for box in self._boxes[:-1]:
+            question = box.get_next_priority_question()
+            if question:
+                return question
+        return None
+
+    def _log_box_counts(self):
+        for box in self._boxes:
+            print(f"{box.name}: {len(box)} questions")
